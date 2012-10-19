a2fadfee563c06172dc56d729efacd2d8a232436
Sentences <nico@mxtracks.de>
2012-10-19 16:36:45 +0200

    changed code style from laravel style to PSR standard style i can read it much better (; in this case, the following methods are changed resize_min changed to resizeMin resize_max changed to resizeMax set_quality changed to setQuality image_info changed to imageInfo

--- classes/filecache.php
+++ classes/filecache.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace ImageManipulationWithGd;
 
-use \Laravel\File as File,
-    \Laravel\Config as Config;
+use \Laravel\File as File;
+use \Laravel\Config as Config;
+
 /**
  * The filecache
  * I might think this needs some work, but for now, it works..
@@ -14,72 +14,72 @@ use \Laravel\File as File,
  */
 class Filecache
 {
-  /**
-   * The full path to the caching dir
-   * @var string 
-   */
-  private static $cache_path = '';
-  /**
-   * The lifetime for the cached file
-   * @var int 
-   */
-  private static $cache_lifetime = 0;
-  
-  /**
-   * Retrieves the settings from the config file
-   */
-  private static function load_config()
-  {
-    static::$cache_path = Config::get('imwg::settings.cache_path', '');
-    static::$cache_lifetime = Config::get('imwg::settings.cache_lifetime', 0);
-  }
 
-  /**
-   * Checks, if the cache has the given file
-   * @param string $file The filename
-   * @param int $lifetime Lifetime of the file in cache
-   * @return boolean false if not found
-   */
-  public static function has($file, $lifetime = 0)
-  {
-    static::load_config();
-    $lifetime = $lifetime > 0 ? $lifetime : static::$cache_lifetime;
-    $file = static::$cache_path . $file;
+    /**
+     * The full path to the caching dir
+     * @var string 
+     */
+    private static $cache_path = '';
 
-    if (File::exists($file))
+    /**
+     * The lifetime for the cached file
+     * @var int 
+     */
+    private static $cache_lifetime = 0;
+
+    /**
+     * Retrieves the settings from the config file
+     */
+    private static function loadConfig()
     {
-      $filemtime = filemtime($file);
-      if ($filemtime + $lifetime < time())
-      {
-        File::delete($file);
-        return false;
-      }
-      return true;
+        static::$cache_path = Config::get('imwg::settings.cache_path', '');
+        static::$cache_lifetime = Config::get('imwg::settings.cache_lifetime', 0);
     }
-  }
 
-  /**
-   * Get the Full imagepath from cache
-   * @param string $file
-   * @return string|boolean 
-   */
-  public static function get_path($file)
-  {
-    static::load_config();
-    $file = static::$cache_path . $file;
-    return $file;
-  }
+    /**
+     * Checks, if the cache has the given file
+     * @param string $file The filename
+     * @param int $lifetime Lifetime of the file in cache
+     * @return boolean false if not found
+     */
+    public static function has($file, $lifetime = 0)
+    {
+        static::loadConfig();
+        $lifetime = $lifetime > 0 ? $lifetime : static::$cache_lifetime;
+        $file = static::$cache_path . $file;
 
-  /**
-   * Creates the filename used for caching
-   * @param string $image The original image path
-   * @param array $option The route_option
-   * @return string The Filename 
-   */
-  public static function retrieve_valid_filename($image, $option)
-  {
-    $ext = File::extension($image);
-    return md5($option . $image) . '.' . $ext;
-  }
+        if (File::exists($file)) {
+            $filemtime = filemtime($file);
+            if ($filemtime + $lifetime < time()) {
+                File::delete($file);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    /**
+     * Get the Full imagepath from cache
+     * @param string $file
+     * @return string|boolean 
+     */
+    public static function getPath($file)
+    {
+        static::loadConfig();
+        $file = static::$cache_path . $file;
+        return $file;
+    }
+
+    /**
+     * Creates the filename used for caching
+     * @param string $image The original image path
+     * @param array $option The route_option
+     * @return string The Filename 
+     */
+    public static function retrieveValidFilename($image, $option)
+    {
+        $ext = File::extension($image);
+        return md5($option . $image) . '.' . $ext;
+    }
 
 }
